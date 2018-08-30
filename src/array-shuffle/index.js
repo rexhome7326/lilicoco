ae8e753f536677f16aa1b424389e92db5e907030
rexhome7326 <rexhome7326@gmail.com>
2018-08-30 21:50:42 +0800

    add left and right

--- src/array-shuffle/index.js
+++ src/array-shuffle/index.js
@@ -1,9 +1,21 @@
 function shuffle(target) {
+  console.log('===');
   if (target.length === 0) return [];
+  if (target.length === 1) return target;
+
   const r = Math.round(Math.random() * (target.length - 1));
   const value = target[r];
   target.splice(r, 1);
-  return [value].concat(shuffle(target));
+
+  if (target.length < 2) {
+    return [value].concat(target);
+  }
+
+  const helf = Math.ceil(target.length / 2);
+  const left = target.slice(0, helf - 1) || [];
+  const right = target.slice(helf - 1, target.length) || [];
+
+  return [value].concat(shuffle(left), shuffle(right));
 }
 const a = [1, 3, 4, 5, 6, 7];
 console.log(shuffle(a));
